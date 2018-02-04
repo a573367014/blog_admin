e5d076096ddb9ffc86b4eb83252683d488d9d733
CF <573367014@qq.com>
2018-02-04 15:22:18 +0800

    dev 2018/2/4

--- src/views/Test.jsx
+++ src/views/Test.jsx
@@ -5,26 +5,15 @@ import stores from '@/store/createStore';
 import routes from '@/routes';
 import {renderRoutes} from 'react-router-config';
 import Login from './Login';
-// import Layout from './Layout';
-// const matchs = matchRoutes(routes, '/article');
-
-class App extends React.Component {
-   render () {
-      console.log(this.props);
-      return (
-         <Switch>
-            <Route path="/login" component={Login} />
-            {!window.isLogin ? renderRoutes(routes) : <Login />}
-         </Switch>
-      );
-   }
-}
 
 export default () => {
    return (
       <Provider {...stores}>
          <Router>
-            <Route component={App} />
+            <Switch>
+               <Route path="/login" component={Login} />
+               {!window.isLogin ? renderRoutes(routes) : <Redirect to="/login" />}
+            </Switch>
          </Router>
       </Provider>
    );
