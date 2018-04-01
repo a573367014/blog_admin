3f4d98443c1d6e1080669df05095a3b2d3c85d75
CF <573367014@qq.com>
2018-04-01 08:03:58 +0800

    1

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const Koa = require('koa');
-const static = require('koa-static');
+const koaStatic = require('koa-static');
+const path = require('path');
 const app = new Koa();
 
 app
-  .use(static(__dirname + '/dist'))
-  .listen(3333);
+   .use(koaStatic(path.join(__dirname, '/dist')))
+   .listen(3333, () => {
+      console.log('成功监听3333端口');
+   });
